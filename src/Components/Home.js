df0ebf6b6c9f06b8b92279baa2dc8dcef9ec3c94
Justin Adams <adamsjustin17@gmail.com>
2020-04-26 05:22:55 -0400

    playing with style and components

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+            <div className="flexslider js-fullheight" id="homeBody">
+                Coming Soon, More Ways to Love the Gamer!
+            </div>
+        </section>
+      </div>
+    )
+  }
+}
